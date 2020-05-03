ab3e0b80036509dd737e05187bde24e11bead01d
kntaka <knt.takahashii@gmail.com>
2020-05-03 18:20:10 +0900

    fix client

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -17,7 +18,20 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
-	fmt.Fprintf(w, validToken)
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:9000/", nil)
+	req.Header.Set("Token", validToken)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err.Error())
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err.Error())
+	}
+	fmt.Fprintf(w, string(body))
 }
 
 func GenerateJWT() (string, error) {
